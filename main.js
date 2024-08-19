7544f0dea060376220066a054162dfa77405f7bf
battistaennio <ennio.battista99@gmail.com>
2024-08-19 09:56:47 +0200

    feat: add getApi method

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            apiUrl: "server.php",
             newTask: "",
+            list: [],
         }
     },
     methods: {
-
+        getApi() {
+            axios.get(this.apiUrl)
+                .then(response => {
+                    this.list = response.data;
+                    console.log(this.list);
+                })
+        }
+    },
+    mounted() {
+        this.getApi()
     }
 }).mount("#container")
